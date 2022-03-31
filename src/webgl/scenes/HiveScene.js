89f0c68b7ecc44f4cb4af827084339a793c252f0
leoniedarois <50202798+leoniedarois@users.noreply.github.com>
2022-03-31 15:24:55 +0200

    [hiveScene]: add bee to point with loop :bee:

--- src/webgl/scenes/HiveScene.js
+++ src/webgl/scenes/HiveScene.js
@@ -19,17 +19,16 @@ export default class HiveScene extends Group {
 
     this.webGl = new WebGl()
     this.scene = this.webGl.scene
+    this.sizes = this.webGl.sizes
     this.resources = this.webGl.resources
     this.camera = this.webGl.camera
 
     this.raycaster = null
     this.currentIntersect = null
 
+    this.beesToPoint = []
+
     this.mouse = new Vector2()
-    this.sizes = {
-      width: window.innerWidth,
-      height: window.innerHeight,
-    }
 
     // Wait for resources
     this.resources.on(`sourcesReadyhive`, () => {
@@ -61,27 +60,6 @@ export default class HiveScene extends Group {
 
     this.bee = new Bee()
 
-    // this.object1 = new Bee()
-    // this.object1.model.position.x = -3
-    // this.object1.model.position.z = 1
-    // this.object1.model.rotation.y = Math.PI
-    // this.object1.model.children[1].testId = 0
-    //
-    //
-    // this.object2 = new Bee()
-    // this.object2.testId = 1
-    //
-    //
-    // this.object3 = new Bee()
-    // this.object3.model.position.x = 2
-    // this.object3.model.position.y = 1.5
-    // this.object3.model.position.z = 2
-    // this.object3.model.children[1].testId = 2
-    //
-    // this.add(this.object1.model, this.object2.model, this.object3.model)
-    //
-    // this.objectsToTest = [this.object1.model, this.object2.model, this.object3.model]
-
     this.raycaster = new Raycaster()
 
     this.hive = this.resources.items.hiveModel.scene
@@ -99,21 +77,16 @@ export default class HiveScene extends Group {
     this.add(this.hive)
 
     // Add bee
-    this.beesToPoint = []
-
     for (let i = 0; i < this.points.length; i++) {
-      const bee = skeletonClone(this.bee.model) 
-      bee.position.x = this.points[i].position.x
-      bee.position.y = this.points[i].position.y
-      bee.position.z = this.points[i].position.z
+      const bee = skeletonClone(this.bee.model)
+      bee.position.set(this.points[i].position.x, this.points[i].position.y, this.points[i].position.z)
+      bee.rotation.y = Math.PI
       bee.children[1].testId = this.points[i].id
 
       this.beesToPoint.push(bee)
-      console.log(bee, this.beesToPoint)
 
       this.add(bee)
     }
-    
 
     // Listener
     window.addEventListener("mousemove", (e) => {
@@ -133,8 +106,8 @@ export default class HiveScene extends Group {
     if (this.points && this.raycaster) {
       this.raycaster.setFromCamera(this.mouse, this.camera)
 
-
-      // this.intersects = this.raycaster.intersectObjects(this.objectsToTest, true) // objects listed
+      // objects to test with the raycaster
+      this.intersects = this.raycaster.intersectObjects(this.beesToPoint, true)
 
       // for (const object of this.objectsToTest) {
       //   object.material.color.set("#C571FF");
@@ -144,30 +117,30 @@ export default class HiveScene extends Group {
       //   intersect.object.material.color.set("#0000ff");
       // }
 
-    //   if (this.intersects.length) {
-    //     if (this.currentIntersect) {
-    //
-    //       this.points[this.currentIntersect.object.testId].element.classList.add('visible')
-    //
-    //       const screenPosition = this.points[this.currentIntersect.object.testId].position.clone()
-    //       screenPosition.project(this.camera)
-    //
-    //       const translateX = screenPosition.x * this.sizes.width * 0.5
-    //       const translateY = -screenPosition.y * this.sizes.height * 0.5
-    //       this.points[this.currentIntersect.object.testId].element.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`
-    //
-    //     }
-    //     this.currentIntersect = this.intersects[0]
-    //   } else {
-    //
-    //     if (!this.currentIntersect) {
-    //       for (const point of this.points) {
-    //         point.element.classList.remove('visible')
-    //       }
-    //
-    //     }
-    //     this.currentIntersect = null
-    //   }
+      if (this.intersects.length) {
+        if (this.currentIntersect) {
+
+          this.points[this.currentIntersect.object.testId].element.classList.add('visible')
+
+          const screenPosition = this.points[this.currentIntersect.object.testId].position.clone()
+          screenPosition.project(this.camera)
+
+          const translateX = screenPosition.x * this.sizes.width * 0.5
+          const translateY = -screenPosition.y * this.sizes.height * 0.5
+          this.points[this.currentIntersect.object.testId].element.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`
+
+        }
+        this.currentIntersect = this.intersects[0]
+      } else {
+
+        if (!this.currentIntersect) {
+          for (const point of this.points) {
+            point.element.classList.remove('visible')
+          }
+
+        }
+        this.currentIntersect = null
+      }
     }
   }
 
