20e74036690db8b7f16ed295b8ec4f5f66de08d5
leoniedarois <50202798+leoniedarois@users.noreply.github.com>
2022-03-31 11:33:19 +0200

    [hive-WIP]: add bee

--- src/webgl/scenes/HiveScene.js
+++ src/webgl/scenes/HiveScene.js
@@ -1,14 +1,13 @@
-import {Group, Mesh, MeshBasicMaterial, SphereGeometry, Vector2, Raycaster, Vector3} from 'three'
+import {Group, Vector2, Raycaster, Vector3} from 'three'
 import WebGl from '../webglManager'
-import Bee from "@/webgl/entities/BlueBee";
+import Bee from "@/webgl/entities/BlueBee"
 
 let hiveInstance = null
 
-export default class HiveScene extends Group
-{
-  constructor(){
-    if(hiveInstance) {
-      return(
+export default class HiveScene extends Group {
+  constructor() {
+    if (hiveInstance) {
+      return (
         hiveInstance
       )
     }
@@ -31,8 +30,7 @@ export default class HiveScene extends Group
     }
 
     // Wait for resources
-    this.resources.on(`sourcesReadyhive`, () =>
-    {
+    this.resources.on(`sourcesReadyhive`, () => {
       this.setup()
     })
   }
@@ -41,47 +39,46 @@ export default class HiveScene extends Group
     this.points = [
       {
         position: new Vector3(-3, 0, 1),
-        element: points[0]
+        element: points[0],
+        id: 0
       },
       {
         position: new Vector3(0, 0, 0),
-        element: points[1]
+        element: points[1],
+        id: 1
       },
       {
-        position: new Vector3(2,  1.5, 2),
-        element: points[2]
+        position: new Vector3(2, 1.5, 2),
+        element: points[2],
+        id: 2
       }
     ]
   }
 
-  setup(){
-    // this.object1 = new Mesh(
-    //   new SphereGeometry(0.5, 16, 16),
-    //   new MeshBasicMaterial({ color: "#C571FF" })
-    // )
+  setup() {
+
+    this.bee = new Bee()
 
     this.object1 = new Bee()
-    this.object1.model.position.x = -3;
-    this.object1.model.position.z = 1;
+    this.object1.model.position.x = -3
+    this.object1.model.position.z = 1
     this.object1.model.rotation.y = Math.PI
-    this.object1.name = "bee";
+    this.object1.model.children[1].testId = 0
 
-    this.object2 = new Mesh(
-      new SphereGeometry(0.5, 16, 16),
-      new MeshBasicMaterial({ color: "#C571FF" })
-    )
-    this.object2.name = 2;
 
-    this.object3 = new Mesh(
-      new SphereGeometry(0.5, 16, 16),
-      new MeshBasicMaterial({ color: "#C571FF" })
-    );
-    this.object3.position.x = 2;
-    this.object3.position.y = 1.5;
-    this.object3.position.z = 2;
-    this.object3.name = 3;
+    this.object2 = new Bee()
+    this.object2.testId = 1
 
-    this.add(this.object1.model, this.object2, this.object3)
+
+    this.object3 = new Bee()
+    this.object3.model.position.x = 2
+    this.object3.model.position.y = 1.5
+    this.object3.model.position.z = 2
+    this.object3.model.children[1].testId = 2
+
+    this.add(this.object1.model, this.object2.model, this.object3.model)
+
+    this.objectsToTest = [this.object1.model, this.object2.model, this.object3.model]
 
     this.raycaster = new Raycaster()
 
@@ -90,12 +87,23 @@ export default class HiveScene extends Group
     this.init()
   }
 
-  init(){
+  init() {
     // Set Camera position
-    this.webGl.camera.position.set(0, 4, -10)
+    this.webGl.camera.position.set(0, 4, -40)
 
-    // Add Hive
+    // Add hive
     this.add(this.hive)
+
+    // Add bee
+    // for (let i = 0; i < this.points.length; i++) {
+    //   const bee = this.bee.model.clone()
+    //   console.log(bee, this.points[i].position.x)
+    //   bee.position.x = this.points[i].position.x
+    //   bee.position.y = this.points[i].position.y
+    //   bee.position.z = this.points[i].position.z
+    //   bee.children[1].testId = this.points[i].id
+    //   this.add(bee)
+    // }
 
     // Listener
     window.addEventListener("mousemove", (e) => {
@@ -104,66 +112,58 @@ export default class HiveScene extends Group
     })
 
     window.addEventListener("click", () => {
-      if(this.currentIntersect) {
+      if (this.currentIntersect) {
         console.log('click on model')
       }
     })
 
   }
 
-  update(){
-    if(this.points && this.raycaster) {
-      for(const point of this.points)
-      {
-        this.raycaster.setFromCamera(this.mouse, this.camera)
+  update() {
+    if (this.points && this.raycaster) {
+      this.raycaster.setFromCamera(this.mouse, this.camera)
 
-        const screenPosition = point.position.clone()
-        screenPosition.project(this.camera)
-        const objectsToTest = [this.object1.model.children[0], this.object2, this.object3];
 
-        // console.log(this.object1, 'bee')
-        console.log(this.scene.children[2])
+      this.intersects = this.raycaster.intersectObjects(this.objectsToTest, true) // objects listed
 
-        const intersects = this.raycaster.intersectObjects(objectsToTest, true) // objects listed
-        // console.log(objectsToTest)
+      // for (const object of this.objectsToTest) {
+      //   object.material.color.set("#C571FF");
+      // }
+      //
+      // for (const intersect of intersects) {
+      //   intersect.object.material.color.set("#0000ff");
+      // }
 
-        // for (const object of objectsToTest) {
-        //   object.material.color.set("#C571FF");
-        // }
-        //
-        // for (const intersect of intersects) {
-        //   intersect.object.material.color.set("#0000ff");
-        // }
+      if (this.intersects.length) {
+        if (this.currentIntersect) {
 
-        if(intersects.length) {
-          console.log(intersects[0].object.name)
-          if(this.currentIntersect) {
+          this.points[this.currentIntersect.object.testId].element.classList.add('visible')
 
-            if(this.currentIntersect && JSON.stringify(this.currentIntersect.object.position) === JSON.stringify(point.position)) {
-              point.element.classList.add('visible')
-            }
+          const screenPosition = this.points[this.currentIntersect.object.testId].position.clone()
+          screenPosition.project(this.camera)
 
-          }
-          this.currentIntersect = intersects[0]
-        } else {
+          const translateX = screenPosition.x * this.sizes.width * 0.5
+          const translateY = -screenPosition.y * this.sizes.height * 0.5
+          this.points[this.currentIntersect.object.testId].element.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`
 
-          if(!this.currentIntersect) {
-            point.element.classList.remove('visible')
-
-          }
-          this.currentIntersect = null
         }
+        this.currentIntersect = this.intersects[0]
+      } else {
 
-        const translateX = screenPosition.x * this.sizes.width * 0.5
-        const translateY = - screenPosition.y * this.sizes.height * 0.5
-        point.element.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`
+        if (!this.currentIntersect) {
+          for (const point of this.points) {
+            point.element.classList.remove('visible')
+          }
+
+        }
+        this.currentIntersect = null
       }
     }
   }
 
-  delete(){
+  delete() {
     window.removeEventListener("click", () => {
-      if(this.currentIntersect) {
+      if (this.currentIntersect) {
         console.log('click on model')
       }
     })
