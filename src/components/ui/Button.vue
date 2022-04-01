059a335f4143841b6e39637f4629a303e0749c7b
leoniedarois <50202798+leoniedarois@users.noreply.github.com>
2022-04-01 12:27:15 +0200

    [general]: - add button on scene - add global scss - create file for scss

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <router-link :to=to>{{label}}</router-link>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  name: 'Button',
+  props: {
+    to: {
+      required: true,
+      default: '/'
+    },
+    label: {
+      required: true,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/HoneyIntro.vue
+++ src/views/HoneyIntro.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="honeyIntro">
     <h1>Honey Home Intro</h1>
+    <Button to="/hive" label="Next"/>
+
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Button from '@/components/ui/Button'
+
+export default {
+  name: 'HoneyIntro',
+  components: {
+    Button
+  }
+}
+</script>
 
 <style scoped lang="scss">
 
