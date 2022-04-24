adeeb434371d32964d45285feafdf7a56db39e26
leoniedarois <50202798+leoniedarois@users.noreply.github.com>
2022-04-24 18:33:55 +0200

    [outside]: terrain WIP

--- src/webgl/shaders/grass.js
+++ src/webgl/shaders/grass.js
@@ -5,14 +5,19 @@ import {
   ConeGeometry,
   ShaderMaterial,
   Group,
-  PlaneGeometry,
+  PlaneBufferGeometry,
   MeshBasicMaterial,
-  Mesh
+  Mesh, MeshStandardMaterial, PlaneGeometry, Vector3
 } from 'three'
 import WebGl from '../webglManager'
 import vertexShader from './grass/vert.glsl'
 import fragmentShader from './grass/frag.glsl'
 import mapSetting from '../elementsLocations/outsideOne/mapSetting.json'
+import simplexNoise from 'simplex-noise'
+import {ImprovedNoise} from 'three/examples/jsm/math/ImprovedNoise'
+
+const simplex = new simplexNoise('mama-leo')
+console.log(simplex)
 
 const uniforms = {
   time: {
@@ -55,19 +60,47 @@ export default class Grass extends Group {
     geometry.translate(0, 0.5, 0) // move grass blade geometry lowest point at 0.
 
     const instancedMesh = new InstancedMesh(geometry, this.leavesMaterial, this.instanceNumber)
-    this.add(instancedMesh)
+    // this.add(instancedMesh)
 
     // Floor
-    this.floor = new Mesh(
-      new PlaneGeometry( mapSetting[0].right / this.property.map.ratio, mapSetting[0].bottom / this.property.map.ratio ),
-      new MeshBasicMaterial({
-        color: 'black',
-        side: DoubleSide
-      })
-    )
+    this.floorGeometry = new PlaneGeometry(mapSetting[0].right / this.property.map.ratio, mapSetting[0].bottom / this.property.map.ratio, 32, 32)
+    this.floorMaterial = new MeshStandardMaterial({
+      color: 'black',
+      side: DoubleSide,
+      wireframe: true
+    })
+
+    this.floor = new Mesh(this.floorGeometry, this.floorMaterial)
+
+    const vertices = this.floorGeometry.getAttribute("position")
+    this.floorGeometry.verticesNeedUpdate = true
+
+    console.log(vertices, 'vertices')
+
+    for (let i = 0; i < vertices.array.length / 3; i++) {
+      const i3 = i * 3
+      const noise = simplex.noise2D(vertices[i3] * 30, vertices[i3 + 1] * 30) + 0.7
+      vertices[i3 + 2] += noise * 0.004
+    }
+
+
+    for (let i = 0; i < vertices.array.length; i++) {
+      let v = new Vector3(
+        i,
+        i + 1,
+        i + 2
+      )
+      console.log(v)
+      v.y = simplex.noise2D(v.x * 0.01, v.y * 0.01) * 30
+      v.y += simplex.noise2D(v.x * 0.03, v.y * 0.03) * 5
+      v.y += simplex.noise2D(v.x * 0.1, v.y * 0.125)
+    }
+
+
     this.floor.name = 'floor'
     this.floor.translateZ(-mapSetting[0].bottom / (this.property.map.ratio * 2))
-    this.floor.rotateX(Math.PI/2)
+    // this.floor.translateY(150)
+    this.floor.rotateX(Math.PI / 2)
     this.add(this.floor)
 
     // Position and scale the grass blade instances randomly.
@@ -75,14 +108,13 @@ export default class Grass extends Group {
       this.dummy.position.set(
         (Math.random() - 0.5) * mapSetting[0].right / this.property.map.ratio,
         0,
-        (Math.random() - 0.5) * (mapSetting[0].bottom / this.property.map.ratio ) - mapSetting[0].bottom / (this.property.map.ratio * 2)
+        (Math.random() - 0.5) * (mapSetting[0].bottom / this.property.map.ratio) - mapSetting[0].bottom / (this.property.map.ratio * 2)
       )
 
       this.dummy.scale.setScalar(0.5 + Math.random() * 1)
       this.dummy.rotation.y = Math.random() * Math.PI
       this.dummy.updateMatrix()
       instancedMesh.setMatrixAt(i, this.dummy.matrix)
-
     }
   }
 
