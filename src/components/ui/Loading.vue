0a52cf1755128a24d2e05792a3a059e5974f21fd
leoniedarois <50202798+leoniedarois@users.noreply.github.com>
2022-05-17 16:19:15 +0200

    [loader]: change loader

--- src/components/ui/Loading.vue
+++ src/components/ui/Loading.vue
@@ -1,6 +1,19 @@
 <template>
   <div ref="loadingScreen" class="loading">
-    <div class="lds-ripple"><div></div><div></div></div>
+<!--    <div class="lds-ripple"><div></div><div></div></div>-->
+    <svg viewBox="0 0 340 333">
+
+      <path class="path" fill="black" stroke="white" stroke-width="4" d="M66.039,133.545c0,0-21-57,18-67s49-4,65,8
+	s30,41,53,27s66,4,58,32s-5,44,18,57s22,46,0,45s-54-40-68-16s-40,88-83,48s11-61-11-80s-79-7-70-41
+	C46.039,146.545,53.039,128.545,66.039,133.545z"/>
+
+    </svg>
+    <p class="u-uppercase loading__text">Chargement ...</p>
+
+    <div class="headphones">
+      <img src="/images/loader/headphone.svg" alt="icon for sound, headphone">
+      <p>Use headphones for better experience</p>
+    </div>
   </div>
 </template>
 
@@ -23,6 +36,24 @@ export default {
 </script>
 
 <style scoped lang="scss">
+svg {
+  height: 165px;
+}
+.path {
+  stroke-dasharray: 600;
+  stroke-dashoffset: 600;
+  animation: dash 1s linear infinite;
+}
+
+@keyframes dash {
+  from {
+    stroke-dashoffset: 822;
+  }
+  to {
+    stroke-dashoffset: 0;
+  }
+}
+
 .loading {
   height: 100vh;
   width: 100vw;
@@ -32,9 +63,29 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
   opacity: 1;
   visibility: visible;
   z-index: 5;
+
+  .headphones {
+    position: absolute;
+    bottom: 48px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    img {
+      margin-bottom: 16px;
+    }
+  }
+
+  &__text {
+    font-family: 'RoadRage', sans-serif;
+    font-size: 32px;
+    color: $white;
+  }
 }
 
 .loaded {
