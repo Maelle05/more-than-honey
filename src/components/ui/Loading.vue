a925275f4317df09cb4a0b41f135ef06f2d1396d
leoniedarois <50202798+leoniedarois@users.noreply.github.com>
2022-05-19 12:32:36 +0200

    [loader]: change loader

--- src/components/ui/Loading.vue
+++ src/components/ui/Loading.vue
@@ -2,7 +2,7 @@
   <div ref="loadingScreen" class="loading">
 <!--    <div class="lds-ripple"><div></div><div></div></div>-->
     <svg viewBox="0 0 340 333">
-
+<!--      en attendant le bon svg de la part des designers -->
       <path class="path" fill="black" stroke="white" stroke-width="4" d="M66.039,133.545c0,0-21-57,18-67s49-4,65,8
 	s30,41,53,27s66,4,58,32s-5,44,18,57s22,46,0,45s-54-40-68-16s-40,88-83,48s11-61-11-80s-79-7-70-41
 	C46.039,146.545,53.039,128.545,66.039,133.545z"/>
