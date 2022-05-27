fe5dc8bf4e1bf1fcd3b804f62284b37a74cff2ae
leoniedarois <50202798+leoniedarois@users.noreply.github.com>
2022-05-27 10:31:19 +0200

    [voice]: split in two files

--- src/utils/voice.js
+++ src/utils/voice.js
@@ -1,79 +1,4 @@
 import EventEmitter from '../webgl/utils/EventEmitter'
-import text from './text.json'
-
-export class SlideSubtitle {
-  constructor(slideStep) {
-    this.voice = text[slideStep]
-    this.finish = false
-    this.numSentences = 0
-    this.duration = 0
-    this.step = 0
-    this.containerSubtitle = document.querySelector<HTMLInputElement>('.subtitles--wrapper')
-    this.textSubtitle = document.createElement('p')
-    this.play = true
-  }
-
-  init() {
-    if (this.containerSubtitle) {
-      this.containerSubtitle.appendChild(this.textSubtitle)
-    }
-    this.step = 0
-    this.play = true
-    this.finish = false
-    this.durationVoice()
-    this.getNewSentence(this.play)
-    this.textSubtitle.classList.remove('hidden')
-  }
-
-  getNewSentence(play) {
-    this.textSubtitle.innerHTML = this.voice[this.step].text
-    const duration = play ? this.voice[this.step].duration * 1000 : 0
-    this.finish = false
-    setTimeout(() => {
-      if (this.voice[this.step + 1] && play === true) {
-        this.step++
-        this.getNewSentence(this.play)
-      } else {
-        this.finish = true
-        this.textSubtitle.innerHTML = ''
-        this.textSubtitle.classList.add('hidden')
-        this.step = 0
-      }
-    }, duration)
-  }
-
-  durationVoice() {
-    let durationVoice = 0
-    let numS = 0
-
-    this.voice.forEach(sentence => {
-      numS++
-      durationVoice = durationVoice + sentence.duration
-    })
-
-    this.numSentences = numS
-    this.duration = durationVoice
-  }
-
-  getDuration() {
-    return this.duration * 1000
-  }
-
-  isFinish() {
-    if (this.finish === true) {
-      return true
-    } else {
-      return false
-    }
-  }
-
-  stop() {
-    this.play = false
-    this.finish = false
-    this.step = 0
-    this.getNewSentence(this.play)
-  }
-}
 
 export class AudioClass extends EventEmitter{
   constructor(url) {
@@ -138,5 +63,4 @@ export class AudioClass extends EventEmitter{
       )
     }
   }
-
 }
