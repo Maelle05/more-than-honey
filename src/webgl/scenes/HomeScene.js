3890e826a04bdbdecf517de15cf08d6259efb563
Maelle05 <maelle.rabouan@gmail.com>
2022-06-11 15:28:31 +0200

    wim

--- src/webgl/scenes/HomeScene.js
+++ src/webgl/scenes/HomeScene.js
@@ -14,20 +14,14 @@ import nenupharLocation from '../elementsLocations/cinematique/nenuphar.json'
 import bridgeLocation from '../elementsLocations/cinematique/bridge.json'
 import mapSetting from '../elementsLocations/mapSetting.json'
 import Listener from '../utils/Listener'
-import gsap, { Back, Elastic } from 'gsap/all'
+import gsap, { Back, Elastic, Power1 } from 'gsap/all'
 import store from '../../store/index'
 import { SlideSubtitle } from '../../utils/audioSubtitles/subtitles'
 import {convertPosition} from '@/webgl/utils/ConvertPosition'
 import hiveLocation from '../elementsLocations/cinematique/hive.json'
 import BlueBee from '../entities/BlueBee'
-import { Vector3 } from 'three'
 
 let Instance = null
-
-const targetsPoints =  [ 
-  new Vector3(-5, 8, 100),
-  new Vector3(0, 7, 105),
-]
 
 export default class HomeScene extends Group
 {
@@ -42,6 +36,8 @@ export default class HomeScene extends Group
     this.webGl = new WebGl()
     this.scene = this.webGl.scene
     this.resources = this.webGl.resources
+
+    this.quaternions = []
 
     // Wait for resources
     this.resources.on(`sourcesReadyhome`, () =>
@@ -179,27 +175,11 @@ export default class HomeScene extends Group
     setTimeout(()=>{
       this.annimBee()
     }, 22000)
+
   }
 
   annimBee(){
     this.goToPoint(0)
-  }
-
-  goToPoint(step){
-    this.bee.model.lookAt(targetsPoints[step])
-    gsap.to(this.bee.model.position, {
-      x: targetsPoints[step].x,
-      y: targetsPoints[step].y,
-      z: targetsPoints[step].z,
-      duration: 5,
-      ease: Back.
-      easeInOut.config(
-      1.7)
-    }).then(()=>{
-      if (targetsPoints[step+1]) {
-        this.goToPoint(step + 1)
-      }
-    })
   }
 
   update(){
