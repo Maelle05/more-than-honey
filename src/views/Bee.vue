04a6b1c5f01ec187c819dc1a7da54065e4bdf8eb
leoniedarois <50202798+leoniedarois@users.noreply.github.com>
2022-06-15 14:08:25 +0200

    [General]: remove old view :)

--- src/views/Bee.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="bee">
-    <h1>This is the bee page</h1>
-  </div>
-</template>
-
-<style scoped lang="scss">
-
-</style>
--- src/views/Fox.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="fox">
-    <h1>This is the fox page</h1>
-  </div>
-</template>
-
-<style scoped lang="scss">
-
-</style>
